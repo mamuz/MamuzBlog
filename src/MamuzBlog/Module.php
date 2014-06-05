985a380120ea6cfa21df250e94900e5f569cc729
mamuz <muzzi_is@web.de>
2014-06-05 22:04:16 +0200

    module fix

--- src/MamuzBlog/Module.php
+++ src/MamuzBlog/Module.php
@@ -25,18 +25,18 @@ class Module implements
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
     {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/../../autoload_classmap.php',
             ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__,
                 ),
             ),
         );
