ed22f8d7b9d072bb0447a0d4b5e8aec7fc66683a
mamuz <muzzi_is@web.de>
2014-08-05 21:47:58 +0200

    code improvement

--- test/MamuzBlogTest/View/Helper/TagListFactoryTest.php
+++ test/MamuzBlogTest/View/Helper/TagListFactoryTest.php
@@ -35,4 +35,27 @@ class TagListFactoryTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('Zend\View\Helper\HelperInterface', $helper);
     }
+
+    public function testCreationWithServiceLocatorAwareness()
+    {
+        $mvcEvent = \Mockery::mock('Zend\Mvc\MvcEvent');
+        $application = \Mockery::mock('Zend\Mvc\Application');
+        $application->shouldReceive('getMvcEvent')->andReturn($mvcEvent);
+        $tagQueryService = \Mockery::mock('MamuzBlog\Feature\TagQueryInterface');
+        $domainManager = \Mockery::mock('Zend\ServiceManager\ServiceLocatorInterface');
+        $domainManager->shouldReceive('get')->with('MamuzBlog\Service\TagQuery')->andReturn($tagQueryService);
+        $sm = \Mockery::mock('Zend\ServiceManager\ServiceLocatorInterface');
+        $sm->shouldReceive('get')->with('MamuzBlog\DomainManager')->andReturn($domainManager);
+        $sm->shouldReceive('get')->with('Application')->andReturn($application);
+
+        $sl = \Mockery::mock('Zend\ServiceManager\AbstractPluginManager');
+        $sl->shouldReceive('getServiceLocator')->andReturn($sm);
+
+        $config = array('mamuz-blog' => array('pagination' => array('range' => array('tag' => 3))));
+        $sm->shouldReceive('get')->with('Config')->andReturn($config);
+
+        $helper = $this->fixture->createService($sl);
+
+        $this->assertInstanceOf('Zend\View\Helper\HelperInterface', $helper);
+    }
 }
